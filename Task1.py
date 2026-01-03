ac702e9c223d72bb7a95907b8e8068694908fde6
Aygian-GH <aaryang9285@gmail.com>
2026-01-03 12:55:46 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,7 @@
+m = int(input("Enter the first number: "))
+n = int(input("Enter the second number: "))
+
+print("\nAddition:", m + n)
+print("Subtraction:", m - n)
+print("Multiplication:", m * n)
+print("Division:", m / n)
